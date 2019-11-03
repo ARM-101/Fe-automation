57b707592099cd6837828236b805c0b94ef25904
ARM-101 <ARM-101@users.noreply.github.com>
2019-11-03 15:42:07 -0800

    second commit

--- src/test/java/com/w2a/testcases/regTest.java
+++ src/test/java/com/w2a/testcases/regTest.java
@@ -22,21 +22,24 @@ public class regTest extends TestBase {
 			driver.findElement(By.cssSelector(OR.getProperty("maleoption"))).click();
 			driver.findElement(By.cssSelector(OR.getProperty("nameInput"))).sendKeys("Melissa");
 			driver.findElement(By.cssSelector(OR.getProperty("zipInput"))).sendKeys("90024");
-			driver.findElement(By.cssSelector(OR.getProperty("submit"))).click();
+			driver.findElement(By.xpath(OR.getProperty("submit"))).click();
 			System.out.println("************");
 			System.out.println("Clicked on Lets go button");
 			System.out.println("************");
 			driver.manage().timeouts().implicitlyWait(Integer.parseInt(config.getProperty("implicit.wait")), TimeUnit.SECONDS);
 			
-			driver.findElement(By.cssSelector(OR.getProperty("emailid"))).sendKeys("ttts8@mailinator.com");
+			driver.findElement(By.cssSelector(OR.getProperty("emailid"))).sendKeys("tttr103@mailinator.com");
 			driver.findElement(By.cssSelector(OR.getProperty("password"))).sendKeys("password123");
 			driver.findElement(By.cssSelector(OR.getProperty("select"))).click();
 			driver.findElement(By.cssSelector(OR.getProperty("Regsubmit"))).click();
 			driver.manage().timeouts().implicitlyWait(Integer.parseInt(config.getProperty("implicit.wait")), TimeUnit.SECONDS);
 			
-			Assert.assertTrue(isElementPresent(By.cssSelector(OR.getProperty("sharebtn"))));
+			//Assert.assertTrue(isElementPresent(By.cssSelector(OR.getProperty("sharebtn"))));
 			
 			driver.findElement(By.cssSelector(OR.getProperty("sharebtn"))).click();
+			
+			//driver.findElement(By.linkText(OR.getProperty("sharebtn_link"))).click();
+			
 			System.out.println("************");
 			System.out.println("Clicked on Share Button");
 			System.out.println("************");
@@ -57,23 +60,25 @@ public class regTest extends TestBase {
 			
 			
 			System.out.println("************");
-			System.out.println("Clicked on child_Continue");
+			System.out.println("Clicked on no.of_child_Continue");
 			System.out.println("************");
 			
 			driver.manage().timeouts().implicitlyWait(Integer.parseInt(config.getProperty("implicit.wait")), TimeUnit.SECONDS);
 			
-			Assert.assertTrue(isElementPresent(By.xpath(OR.getProperty("live_continue_xpath"))));
-			
-			WebElement element1= driver.findElement(By.xpath(OR.getProperty("live_continue_xpath")));
+			//Assert.assertTrue(isElementPresent(By.xpath(OR.getProperty("live_continue_xpath"))));
+			Thread.sleep(3000);
+			//WebElement element1= driver.findElement(By.xpath(OR.getProperty("live_continue_xpath")));
+			WebElement element1=driver.findElement(By.xpath("/html[1]/body[1]/section[2]/main[1]/footer[1]/button[2]"));
+			//new WebDriverWait(driver, 15).until(ExpectedConditions.elementToBeClickable(element1));
 			Actions actions1 = new Actions(driver);
 			actions1.moveToElement(element1).click().build().perform();
 			//driver.findElement(By.className(OR.getProperty("continue_class"))).click();
 			
 			System.out.println("************");
-			System.out.println("Clicked on live_Continue");
+			System.out.println("Clicked on city_live_Continue");
 			System.out.println("************");
-			
-			Thread.sleep(3000);
+			driver.manage().timeouts().implicitlyWait(Integer.parseInt(config.getProperty("implicit.wait")), TimeUnit.SECONDS);
+			//Thread.sleep(3000);
 			Select dob_month = new Select(driver.findElement(By.xpath(OR.getProperty("month"))));
 			
 			//dob_month.selectByValue("03");
@@ -88,9 +93,30 @@ public class regTest extends TestBase {
 			
 			driver.findElement(By.xpath(OR.getProperty("DOB_continue"))).click();
 			
+			System.out.println("************");
+			System.out.println("Clicked on dob_Continue");
+			System.out.println("************");
+			driver.manage().timeouts().implicitlyWait(Integer.parseInt(config.getProperty("implicit.wait")), TimeUnit.SECONDS);
+			
 			driver.findElement(By.xpath(OR.getProperty("ethnic_white"))).click();
 			driver.manage().timeouts().implicitlyWait(Integer.parseInt(config.getProperty("implicit.wait")), TimeUnit.SECONDS);
-			driver.findElement(By.xpath(OR.getProperty("spirit_christian"))).click();	
+			
+			System.out.println("************");
+			System.out.println("Clicked on ethnic_white");
+			System.out.println("************");
+			
+			//driver.findElement(By.xpath(OR.getProperty("spirit_christian"))).click();	
+			WebElement element2 = driver.findElement(By.xpath(OR.getProperty("spirit_christian")));
+			WebDriverWait wait = new WebDriverWait(driver, 20); 
+			
+			wait.until(ExpectedConditions.visibilityOf(element2)); //this will wait for elememt to be visible for 20 seconds
+			element2.click(); //now it clicks on element
+			
+			System.out.println("************");
+			System.out.println("Clicked on spirit_christian");
+			System.out.println("************");
+			
+			
 			driver.manage().timeouts().implicitlyWait(Integer.parseInt(config.getProperty("implicit.wait")), TimeUnit.SECONDS);
 			driver.findElement(By.xpath(OR.getProperty("denom_pefer_not_to_specify"))).click();
 			driver.manage().timeouts().implicitlyWait(Integer.parseInt(config.getProperty("implicit.wait")), TimeUnit.SECONDS);
